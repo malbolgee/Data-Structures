76aba1803a107d60e1fb87592a520071fb66cddd
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-05-12 20:23:32 -0400

    function headers

--- Queue/queue.h
+++ Queue/queue.h
@@ -15,8 +15,12 @@ typedef struct __queue__type{
 
 } queue_t;
 
+#define __INF 0x3F3F3F3F
+
 void make__queue(queue_t *__queue);
 void push__queue(queue_t *__queue, const int __data);
-int queue__front(queue_t *__queue);
-queue_node * queue__pop(queue_t *__queue);
-bool is__empty__queue(queue_t *__queue);
+int queue__front(const queue_t *__queue);
+int dequeue(queue_t *__queue);
+bool is__empty__queue(const queue_t *__queue);
+void print__queue(const queue_t *__queue);
+void array__to__queue(queue_t *__queue, const int *__array, const unsigned __size);
