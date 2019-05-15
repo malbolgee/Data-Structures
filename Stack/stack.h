087185bfa089bbf8e08177ec69e3e5880d11aac9
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-05-15 12:19:14 -0400

    minor changes

--- Stack/stack.h
+++ Stack/stack.h
@@ -15,8 +15,9 @@ typedef struct __stack__type{
 } stack_t;
 
 void push__stack(stack_t *__stack, const int __data);
-int top__stack(stack_t *__stack);
+int top__stack(const stack_t *__stack);
 void make__stack(stack_t *__stack);
-bool is__empty__stack(stack_t *__stack);
+bool is__empty__stack(const stack_t *__stack);
 void pop__stack(stack_t *__stack);
-void print__stack(stack_t *__stack);
+void print__stack(const stack_t *__stack);
+void array__to__stack(stack_t *__stack, const int *__array, const unsigned __size);
